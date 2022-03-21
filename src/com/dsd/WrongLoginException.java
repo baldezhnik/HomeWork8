f3bbd371c7943a4e73176c245c1fd9938d18ceb6
baldezhnik <drako.dima@gmail.com>
2022-03-21 21:34:34 +0300

    Created WrongLoginException

---
+++ src/com/dsd/WrongLoginException.java
@@ -0,0 +1,10 @@
+package com.dsd;
+
+public class WrongLoginException extends Exception{
+    public WrongLoginException() {
+    }
+
+    public WrongLoginException(String message) {
+        super(message);
+    }
+}
